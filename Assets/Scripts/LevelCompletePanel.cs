4b63d883f6a3ec1710f07c4d9bbdbb0430168acc
DESKTOP-7TCGN9E\Administrator <vuong.phan1269@gmail.com>
2023-12-28 22:26:49 +0700

    #commit

---
+++ Assets/Scripts/LevelCompletePanel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Game;
+using dotmob;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelCompletePanel : ShowHidable
+{
+    [SerializeField] private Image _toastTxt;
+    [SerializeField] private TextMeshProUGUI _levelTxt;
+    [SerializeField] private List<string> _toasts = new List<string>();
+
+
+
+    private void Awake()
+    {
+        _levelTxt.text = $" Level {LevelManager.Instance.Level.no}"; 
+    }
+
+    protected override void OnShowCompleted()
+    {
+        base.OnShowCompleted();
+        // _toastTxt.text = _toasts.GetRandom();
+        _toastTxt.gameObject.SetActive(true);
+
+        AdsManager.ShowOrPassAdsIfCan();
+    }
+
+
+    public void OnClickContinue()
+    {
+        UIManager.Instance.LoadNextLevel();
+    }
+
+    public void OnClickMainMenu()
+    {
+        GameManager.LoadScene("MainMenu");
+        SharedUIManager.PausePanel.Hide();
+    }
+}
---
+++ Assets/Scripts/LevelTileUI.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LevelTileUI : MonoBehaviour,IPointerClickHandler
+{
+    public event Action<LevelTileUI> Clicked;
+
+    [SerializeField] private TextMeshProUGUI _txt;
+    [SerializeField] private GameObject _completeMark;
+    [SerializeField] private GameObject _lockMark;
+    [SerializeField] private Image _fillImg;
+
+    private ViewModel _mViewModel;
+
+
+    public ViewModel MViewModel
+    {
+        get => _mViewModel;
+        set
+        {
+            _txt.text = value.Level.no.ToString();
+            _fillImg.color = _fillImg.color.WithAlpha(value.Locked ? 0 : 1);
+            _completeMark.SetActive(value.Completed);
+            _lockMark.SetActive(value.Locked);
+            _mViewModel = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Clicked?.Invoke(this);
+    }
+
+
+    public struct ViewModel
+    {
+        public Level Level { get; set; }
+        public bool Locked { get; set; }
+        public bool Completed { get; set; }
+    }
+}
